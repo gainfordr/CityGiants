2f4ce6816a2481046be1ed356f6e936c3f3d3081
PizzaReaver <rose0188@algonquinlive.com>
2017-09-09 11:12:43 -0400

    - UI Manager    *Created the UI Manager Class    *Created the SetupManager, InitMenus, UpdateStats, EndOfTurn and SwitchMenu
    - BaseMenu
       *Created the BaseMenu item to be  wrapper for menus and other variables
    
    - DistrictMenu, WorkerMenu, BuildingMenu.cs
       *Created class, derives from BaseMenu

---
+++ CityGiants/Assets/Scripts/Manager/UIManager.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class UIManager : MonoBehaviour {
+
+    #region Member Variables
+
+    public Canvas cCurrentCanvas { get; private set; }
+    public List<BaseMenu> lMenuList { get; private set; }
+    public Text tFoodText { get; private set; }
+    public Text tWoodText { get; private set; }
+    public Text tMineText { get; private set; }
+
+    #endregion
+
+
+    #region Functions
+
+    void Awake()
+    {
+
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        //Will be used to check for end of turn and apply end of turn effects
+        UpdateStats();
+	}
+
+    public void SetupManagers()
+    {
+        InitMenus();
+        SwitchMenu(lMenuList[0].cMenu);
+    }
+
+    public void InitMenus()
+    {
+        lMenuList = new List<BaseMenu>();
+    }
+
+    public void SwitchMenu(Canvas aNewMenu)
+    {
+        //Switches menu based on the canvas given to it
+        cCurrentCanvas = aNewMenu;
+    }
+
+    public void SendString(string aString)
+    {
+        //Will send a string to a specific manager
+    }
+
+    public void SendInt(int aInt)
+    {
+        //Will send a int to a specific manager
+    }
+
+    public void UpdateStats()
+    {
+        //Will constantly be keeping track of the menu items for the bar
+    }
+
+    public void EndOfTurn()
+    {
+        //Will deal with UI end of turn specific details
+    }
+
+    #endregion 
+}
---
+++ CityGiants/Assets/Scripts/UI/BaseMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BaseMenu {
+
+    #region Member Variables
+
+    public List<Text> tTextItems { get; private set; }
+    public Canvas cMenu { get; private set; }
+
+    #endregion
+
+}
